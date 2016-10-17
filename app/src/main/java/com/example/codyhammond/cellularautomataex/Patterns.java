ccc64305d03f07733f75761e2532942ff811827d
codyhammond <chammond23@rocketmail.com>
2016-10-17 15:42:56 -0500

    n/a

--- app/src/main/java/com/example/codyhammond/cellularautomataex/Patterns.java
+++
@@ -1,9 +0,0 @@
-package com.example.codyhammond.cellularautomataex;
-
-/**
- * Created by codyhammond on 10/5/16.
- */
-public enum Patterns
-{
-    Sierpiński_triangle;
-}
--- app/src/main/java/com/example/codyhammond/cellularautomataex/RuleSets.java
+++
@@ -1,9 +0,0 @@
-package com.example.codyhammond.cellularautomataex;
-
-/**
- * Created by codyhammond on 10/5/16.
- */
-public class RuleSets
-{
-    public static final short [] Sierpiński_Triangle={0,1,0,1,1,0,1,0};
-}
